c1dfabc1b2c5c339d64812d161d5678e69d2e7f0
Vladimir Lipkin <lipkin@yandex-team.ru>
2024-06-20 13:17:03 +0000

    Pull request #6: Add rude async batch example
    Merge in CLOUD/yandex-cloud-ml-sdk from async_batch_example to master
    
    Squashed commit of the following:
    
    commit 598d60ba516dcaa7b76e634c126deac66d4288d0
    Author: Vladimir Lipkin <lipkin@yandex-team.ru>
    Date:   Wed Jun 19 17:08:45 2024 +0300
    
        Fix logic
    
    commit 998af8e09208971c825488a0b6dc692abe89377d
    Author: Vladimir Lipkin <lipkin@yandex-team.ru>
    Date:   Tue Jun 18 19:43:01 2024 +0300
    
        Improve example
    
    commit 6f521e66fa5e6c68c4fd71e873a0b4e8cebbfdc7
    Author: Vladimir Lipkin <lipkin@yandex-team.ru>
    Date:   Tue Jun 18 16:24:27 2024 +0300
    
        Add rude async batch example

---
+++ examples/completions/async_batch_async.py
@@ -0,0 +1,100 @@
+from __future__ import annotations
+
+import asyncio
+import itertools
+
+import tqdm
+
+from yandex_cloud_ml_sdk import AsyncYCloudML
+
+
+def chunker(it, size):
+    iterator = iter(it)
+    while chunk := list(itertools.islice(iterator, size)):
+        yield chunk
+
+
+async def run_chunked_tasks(function, data, chunk_size, tqdm_callback=None):
+    all_tasks = []
+    for chunk in chunker(data, chunk_size):
+        chunk_tasks = (asyncio.create_task(function(item)) for item in chunk)
+        all_tasks.extend(chunk_tasks)
+
+        if tqdm_callback:
+            tqdm_callback(len(chunk))
+
+        await asyncio.sleep(1)
+
+    results = await asyncio.gather(*all_tasks, return_exceptions=True)
+    for result in results:
+        if isinstance(result, BaseException):
+            raise result
+
+    return results
+
+
+async def main():
+    """
+    Example of running a lot of async requests to yandexgpt with using of our SDK
+    and python asyncio.
+
+    There are further reserve for optimization,
+    like making run_chunked_tasks into async generator
+    and starting status polling right after the task successful creation
+    without waiting for all task to create.
+    But in this case, there will be additional code complication related with
+    several progressbars and with maintaining the results order
+    """
+
+    sdk = AsyncYCloudML(folder_id='b1ghsjum2v37c2un8h64')
+
+    model = sdk.models.completions('yandexgpt')
+
+    input_data = [["foo"]] * 200
+
+    with tqdm.tqdm(total=len(input_data), desc='creatig tasks') as t:
+        operations = await run_chunked_tasks(
+            function=model.run_async,
+            data=input_data,
+            chunk_size=40,
+            tqdm_callback=t.update,
+        )
+
+    running_operations = [(i, operation) for i, operation in enumerate(operations)]
+    finished_operations = []
+    errors = []
+
+    with tqdm.tqdm(total=len(running_operations), desc='finishing tasks') as t:
+        while running_operations:
+            statuses = await run_chunked_tasks(
+                function=lambda i_op: i_op[1].get_status(),
+                data=running_operations,
+                chunk_size=100,
+            )
+
+            for indexed_op, status in zip(running_operations[:], statuses):
+                if not status.done:
+                    continue
+
+                running_operations.remove(indexed_op)
+                t.update(1)
+
+                if status.is_succeeded:
+                    finished_operations.append(indexed_op)
+                else:
+                    errors.append(status.error)
+
+    sorted_operations = (op for _, op in sorted(finished_operations))
+    with tqdm.tqdm(total=len(input_data), desc='fetching results') as t:
+        results = await run_chunked_tasks(
+            function=lambda op: op.get_result(),
+            data=sorted_operations,
+            chunk_size=100,
+            tqdm_callback=t.update
+        )
+
+    print(f'first result: {results[0]}')
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
