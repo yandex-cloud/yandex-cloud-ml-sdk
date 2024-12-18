30ec5a9d042a3e77cd4c5e235b779201fd3aa7a8
Vladimir Lipkin <lipkin@yandex-team.ru>
2024-12-18 18:09:31 +0300

    Add sync example

--- examples/sync/assistants/hybrid_search_index.py
+++ examples/sync/assistants/hybrid_search_index.py
@@ -2,11 +2,10 @@
 
 from __future__ import annotations
 
-import asyncio
 import pathlib
 import pprint
 
-from yandex_cloud_ml_sdk import AsyncYCloudML
+from yandex_cloud_ml_sdk import YCloudML
 from yandex_cloud_ml_sdk.search_indexes import (
     HybridSearchIndexType, ReciprocalRankFusionIndexCombinationStrategy, StaticIndexChunkingStrategy,
     TextSearchIndexType, VectorSearchIndexType
@@ -17,30 +16,29 @@ def local_path(path: str) -> pathlib.Path:
     return pathlib.Path(__file__).parent / path
 
 
-async def main() -> None:
-    sdk = AsyncYCloudML(folder_id='b1ghsjum2v37c2un8h64')
+def main() -> None:
+    sdk = YCloudML(folder_id='b1ghsjum2v37c2un8h64')
 
-    file_coros = (
-        sdk.files.upload(
+    files = []
+    for path in ['turkey_example.txt', 'maldives_example.txt']:
+        file = sdk.files.upload(
             local_path(path),
             ttl_days=5,
             expiration_policy="static",
         )
-        for path in ['turkey_example.txt', 'maldives_example.txt']
-    )
-    files = await asyncio.gather(*file_coros)
+        files.append(file)
 
     # How to create search index with all default settings:
-    operation = await sdk.search_indexes.create_deferred(
+    operation = sdk.search_indexes.create_deferred(
         files,
         index_type=HybridSearchIndexType()
     )
-    default_search_index = await operation.wait()
+    default_search_index = operation.wait()
     print("new hybrid search index with default settings:")
     pprint.pprint(default_search_index)
 
     # But you could override any default:
-    operation = await sdk.search_indexes.create_deferred(
+    operation = sdk.search_indexes.create_deferred(
         files,
         index_type=HybridSearchIndexType(
             chunking_strategy=StaticIndexChunkingStrategy(
@@ -58,7 +56,7 @@ async def main() -> None:
             )
         )
     )
-    search_index = await operation.wait()
+    search_index = operation.wait()
     print("new hybrid search index with overridden settings:")
     pprint.pprint(search_index)
 
@@ -67,12 +65,12 @@ async def main() -> None:
 
     # Created resources cleanup:
     for file in files:
-        await file.delete()
+        file.delete()
 
     for search_index in [default_search_index, search_index]:
         print(f"delete {search_index.id=}")
-        await search_index.delete()
+        search_index.delete()
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
