06a651c54b61cb729229106c4b12dd41b7b0cc96
Vladimir Lipkin <lipkin@yandex-team.ru>
2025-01-29 22:06:57 +0300

    Add a base class for all of proto-enum wrappers

--- src/yandex_cloud_ml_sdk/_search_indexes/normalization_strategy.py
+++ src/yandex_cloud_ml_sdk/_search_indexes/normalization_strategy.py
@@ -5,14 +5,10 @@ import enum
 
 from yandex.cloud.ai.assistants.v1.searchindex.common_pb2 import NormalizationStrategy
 
+from yandex_cloud_ml_sdk._utils.proto import ProtoEnumBase
 
-class IndexNormalizationStrategy(enum.IntEnum):
+
+class IndexNormalizationStrategy(ProtoEnumBase, enum.IntEnum):
     NORMALIZATION_STRATEGY_UNSPECIFIED = NormalizationStrategy.NORMALIZATION_STRATEGY_UNSPECIFIED
     MIN_MAX = NormalizationStrategy.MIN_MAX
     L2 = NormalizationStrategy.L2
-
-    @classmethod
-    def _coerce(cls, strategy: str | int | IndexNormalizationStrategy) -> IndexNormalizationStrategy:
-        if isinstance(strategy, str):
-            strategy = NormalizationStrategy.Value(strategy.upper())
-        return cls(strategy)
