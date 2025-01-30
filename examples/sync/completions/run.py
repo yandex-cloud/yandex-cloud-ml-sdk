75b3f771dcb8c599ddd24bfdeeecb66753e2645a
Vladimir Lipkin <lipkin@yandex-team.ru>
2025-01-30 18:32:37 +0300

    Add reasoning mode for completions

--- examples/sync/completions/run.py
+++ examples/sync/completions/run.py
@@ -8,10 +8,20 @@ from yandex_cloud_ml_sdk import YCloudML
 def main() -> None:
     sdk = YCloudML(folder_id='b1ghsjum2v37c2un8h64')
 
-    result = sdk.models.completions('yandexgpt').configure(temperature=0.5).run("foo")
+    model = sdk.models.completions('yandexgpt')
+
+    result = model.configure(
+        temperature=0.5,
+    ).run("how to calculate the Hirsch index in O(N)")
 
     for alternative in result:
-        print(alternative)
+        print(alternative.text)
+
+    result = model.configure(
+        temperature=0.5,
+        reasoning_mode='enabled_hidden',
+    ).run("how to calculate the Hirsch index in O(N)")
+    print(result[0].text)
 
 
 if __name__ == '__main__':
