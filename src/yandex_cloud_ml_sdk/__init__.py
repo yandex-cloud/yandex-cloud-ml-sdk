f00e7a619a3cdeebd11b4684a1a28d10895b232b
Vladimir Lipkin <lipkin@yandex-team.ru>
2025-02-27 18:55:18 +0300

    Bump version

--- src/yandex_cloud_ml_sdk/__init__.py
+++ src/yandex_cloud_ml_sdk/__init__.py
@@ -4,7 +4,7 @@ from ._logging import setup_default_logging
 from ._logging import setup_default_logging_from_env as _setup_default_logging_from_env
 from ._sdk import AsyncYCloudML, YCloudML
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 
 __all__ = [
     '__version__',
