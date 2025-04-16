a31a0325e138be1b64565d43a26b7f8ed8c70db7
Vladimir Lipkin <lipkin@yandex-team.ru>
2025-04-16 20:44:22 +0300

    Improve batch examples a lot

--- examples/sync/completions/batch.py
+++ examples/sync/completions/batch.py
@@ -2,10 +2,9 @@
 
 from __future__ import annotations
 
-import asyncio
 import pathlib
 
-from yandex_cloud_ml_sdk import AsyncYCloudML
+from yandex_cloud_ml_sdk import YCloudML
 
 PATH = pathlib.Path(__file__)
 NAME = f'example-{PATH.parent.name}-{PATH.name}'
@@ -15,25 +14,25 @@ def local_path(path: str) -> pathlib.Path:
     return pathlib.Path(__file__).parent / path
 
 
-async def get_dataset(sdk):
+def get_dataset(sdk):
     """
     This function represents getting or creating dataset object.
 
     In real life you could use just a datasets ids, for example:
 
     ```
-    dataset = await sdk.datasets.get("some_id")
+    dataset = sdk.datasets.get("some_id")
 
-    batch_task = await model.batch.run_deferred(dataset)
+    batch_task = model.batch.run_deferred(dataset)
     # or just
-    batch_task = await model.batch.run_deferred("dataset_id")
+    batch_task = model.batch.run_deferred("dataset_id")
     ```
 
     To get to know better how to work with datasets,
     please refer to special "datasets/" examples.
     """
 
-    async for dataset in sdk.datasets.list(status='READY', name_pattern=NAME):
+    for dataset in sdk.datasets.list(status='READY', name_pattern=NAME):
         print(f'using old dataset {dataset=}')
         break
     else:
@@ -45,19 +44,19 @@ async def get_dataset(sdk):
             name=NAME,
         )
 
-        dataset = await dataset_draft.upload()
+        dataset = dataset_draft.upload()
         print(f'created new dataset {dataset=}')
 
     return dataset
 
 
-async def main() -> None:
-    sdk = AsyncYCloudML(folder_id='b1ghsjum2v37c2un8h64')
+def main() -> None:
+    sdk = YCloudML(folder_id='b1ghsjum2v37c2un8h64')
     sdk.setup_default_logging()
 
     # This is "usual" dataset as for tuning, but it have
     # to have proper "task_type" to work with batch runs
-    dataset = await get_dataset(sdk)
+    dataset = get_dataset(sdk)
 
     # There are only specific models supports batch runs,
     # please refer to Yandex Foundation Models doctumentation
@@ -65,9 +64,9 @@ async def main() -> None:
 
     # Batch run returns an operation object you could
     # follow or just call .wait method
-    operation = await model.batch.run_deferred(dataset)
+    operation = model.batch.run_deferred(dataset)
 
-    resulting_dataset = await operation
+    resulting_dataset = operation.wait()
 
     # NB: Resulting dataset have task_type="TextToTextGeneration" and could be used as an input
     # for tuning.
@@ -76,14 +75,13 @@ async def main() -> None:
         import pyarrow  # pylint: disable=import-outside-toplevel,unused-import
 
         print('Resulting dataset lines:')
-        async for line in resulting_dataset.read():
+        for line in resulting_dataset.read():
             print(line)
     except ImportError:
         print('skipping dataset read; istall yandex-cloud-ml-sdk[datasets] to be able to read')
 
     # Removing final dataset to not to increase chaos.
-    await resulting_dataset.delete()
-
+    resulting_dataset.delete()
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
