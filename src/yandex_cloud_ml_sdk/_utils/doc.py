b64efe32d8c1cdc4bf7e3371438460c971c8fa1f
Vladimir Lipkin <lipkin@yandex-team.ru>
2025-05-15 15:41:18 +0200

    Add doc_from util (#97)

---
+++ src/yandex_cloud_ml_sdk/_utils/doc.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from typing import Callable, Union
+
+from typing_extensions import TypeAlias
+
+DocSourceType: TypeAlias = Union[type, Callable]
+
+
+def doc_from(source: DocSourceType, **kwargs) -> Callable[[DocSourceType], DocSourceType]:
+    def decorator(destination: DocSourceType) -> DocSourceType:
+        assert type(source) is type(destination), \
+            "docstring must be copied from class to class or from method to method"
+
+        doc = source.__doc__
+
+        assert doc, 'source docstring cannot be empty'
+        assert not destination.__doc__, 'destination docstring must be empty'
+
+        if kwargs:
+            doc = doc.format(**kwargs)
+
+        destination.__doc__ = doc
+
+        return destination
+
+    return decorator
