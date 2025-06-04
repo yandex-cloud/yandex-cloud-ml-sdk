004c94ba088fdc078cfb8a4915ed5381ce9b20c4
Radmila M. <mila.mana@yandex.ru>
2025-06-04 14:54:16 +0300

    Add docstrings for _models/__init__.py

--- src/yandex_cloud_ml_sdk/_models/__init__.py
+++ src/yandex_cloud_ml_sdk/_models/__init__.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 
 from yandex_cloud_ml_sdk._types.domain import DomainWithFunctions
+from yandex_cloud_ml_sdk._utils.doc import doc_from
 
 from .completions.function import AsyncCompletions, BaseCompletions, Completions
 from .image_generation.function import AsyncImageGeneration, ImageGeneration
@@ -10,10 +11,17 @@ from .text_embeddings.function import AsyncTextEmbeddings, TextEmbeddings
 
 
 class BaseModels(DomainWithFunctions):
+    """This class provides a structure for model-related functionalities,
+    including completions and text classifiers. It initializes model
+    functions based on defined annotations.
+    """
+    #: Completions doc
     completions: BaseCompletions
+    #: Text_classifiers doc
     text_classifiers: BaseTextClassifiers
 
 
+@doc_from(BaseModels)
 class AsyncModels(BaseModels):
     completions: AsyncCompletions
     text_embeddings: AsyncTextEmbeddings
@@ -21,6 +29,7 @@ class AsyncModels(BaseModels):
     image_generation: AsyncImageGeneration
 
 
+@doc_from(BaseModels)
 class Models(BaseModels):
     completions: Completions
     text_embeddings: TextEmbeddings
