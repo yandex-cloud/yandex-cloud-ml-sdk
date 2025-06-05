b0df8e43b52e397154892c640ce9f5551dd6e73e
Radmila M. <mila.mana@yandex.ru>
2025-06-05 17:48:42 +0300

    Add docstrings for __init__.py - v.6

--- src/yandex_cloud_ml_sdk/_models/__init__.py
+++ src/yandex_cloud_ml_sdk/_models/__init__.py
@@ -10,6 +10,7 @@ from .text_classifiers.function import AsyncTextClassifiers, BaseTextClassifiers
 from .text_embeddings.function import AsyncTextEmbeddings, BaseTextEmbeddings, TextEmbeddings
 
 
+
 class BaseModels(DomainWithFunctions):
     """
     Domain for working with `Yandex Foundation Models <https://yandex.cloud/ru/services/foundation-models>`.
