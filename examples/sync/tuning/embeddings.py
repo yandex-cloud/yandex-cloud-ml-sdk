9b865386ba390fbc17bed1591833f53b2b361ded
Andrew <kirilenko.haa@gmail.com>
2025-06-17 16:59:44 +0300

    Fix embeddings tuning example (#107)

--- examples/sync/tuning/embeddings.py
+++ examples/sync/tuning/embeddings.py
@@ -47,7 +47,7 @@ def get_datasets(sdk, name, dataset_function):
 def main() -> None:
     sdk = YCloudML(folder_id='b1ghsjum2v37c2un8h64')
     sdk.setup_default_logging()
-    base_model = sdk.models.text_embeddings('yandexgpt-lite')
+    base_model = sdk.models.text_embeddings('text-embeddings')
 
     for name, tune_type, dataset_function in [
         ('embeddings_pair', 'pair', sdk.datasets.text_embeddings_pair),
