38f672fded1354165223dd26474e53a4deb65f4b
Radmila M. <mila.mana@yandex.ru>
2025-06-26 21:54:16 +0300

    Add docstrings for _models/image_generation/function.py
    First version is ready for review

--- src/yandex_cloud_ml_sdk/_models/image_generation/function.py
+++ src/yandex_cloud_ml_sdk/_models/image_generation/function.py
@@ -3,11 +3,25 @@ from __future__ import annotations
 from typing_extensions import override
 
 from yandex_cloud_ml_sdk._types.function import BaseModelFunction, ModelTypeT
+from yandex_cloud_ml_sdk._utils.doc import doc_from
 
 from .model import AsyncImageGenerationModel, ImageGenerationModel
 
 
 class BaseImageGeneration(BaseModelFunction[ModelTypeT]):
+    """
+    Base class for image generation models.
+
+    It provides the functionality to call an image generation model by constructing 
+    the appropriate URI based on the provided model name and version.
+
+    Returns a model's object through which requests to the backend are made.
+
+    Example:
+       >>> model = sdk.models.image_generation('yandex-art')
+       This is how the model is created.
+
+    """
     @override
     def __call__(
         self,
@@ -15,6 +29,17 @@ class BaseImageGeneration(BaseModelFunction[ModelTypeT]):
         *,
         model_version: str = 'latest',
     ):
+        """
+        Call the image generation model with the specified name and version.
+
+        Constructs the URI for the model based on the provided model's name
+        and version. If the name contains '://', it is treated as a
+        complete URI. Otherwise, it constructs a URI using the folder ID
+        from the SDK.
+
+        :param model_name: the name of the image generation model.
+        :param model_version: the version of the model to use (default is 'latest').
+        """
         if '://' in model_name:
             uri = model_name
         else:
@@ -26,10 +51,10 @@ class BaseImageGeneration(BaseModelFunction[ModelTypeT]):
             uri=uri,
         )
 
-
+@doc_from(BaseImageGeneration)
 class ImageGeneration(BaseImageGeneration):
     _model_type = ImageGenerationModel
 
-
+@doc_from(BaseImageGeneration)
 class AsyncImageGeneration(BaseImageGeneration):
     _model_type = AsyncImageGenerationModel
