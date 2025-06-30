ba070de1be3bd9c666d6d64528c35cefd5d22378
GBocharov <bocharovgleb@yandex-team.ru>
2025-07-01 00:37:45 +0300

    useless import removed

--- src/yandex_cloud_ml_sdk/_models/text_classifiers/result.py
+++ src/yandex_cloud_ml_sdk/_models/text_classifiers/result.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from typing import TYPE_CHECKING, Generic, Sequence, TypeVar, cast, overload
 
 from typing_extensions import Self
