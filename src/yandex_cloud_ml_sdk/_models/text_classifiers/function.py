4bfb1a0786b78c7438153edbc48bcd1b4bd80229
Radmila M. <mila.mana@yandex.ru>
2025-07-07 15:48:53 +0300

    Add docstrings for _models/text_classifiers/function.py - v1

--- src/yandex_cloud_ml_sdk/_models/text_classifiers/function.py
+++ src/yandex_cloud_ml_sdk/_models/text_classifiers/function.py
@@ -3,11 +3,17 @@ from __future__ import annotations
 from typing_extensions import override
 
 from yandex_cloud_ml_sdk._types.function import BaseModelFunction, ModelTypeT
+from yandex_cloud_ml_sdk._utils.doc import doc_from
 
 from .model import AsyncTextClassifiersModel, TextClassifiersModel
 
 
 class BaseTextClassifiers(BaseModelFunction[ModelTypeT]):
+    """A class for text classifiers.
+
+    It provides a common interface for text classification models and
+    constructs the model URI based on the provided model name and version.
+    """
     @override
     def __call__(
         self,
@@ -15,6 +21,16 @@ class BaseTextClassifiers(BaseModelFunction[ModelTypeT]):
         *,
         model_version: str = 'latest',
     ):
+        """Call the text classification model.
+
+        Constructs the URI for the model and initializes it
+        and returns an instance of the model type initialized
+        with the SDK and URI.
+
+        :param model_name: the name of the model to be used.
+        :param model_version: the version of the model to be used.
+            Defaults to 'latest'.
+        """
         if '://' in model_name:
             uri = model_name
         else:
@@ -27,9 +43,10 @@ class BaseTextClassifiers(BaseModelFunction[ModelTypeT]):
         )
 
 
+@doc_from(BaseTextClassifiers)
 class TextClassifiers(BaseTextClassifiers):
     _model_type = TextClassifiersModel
 
-
+@doc_from(BaseTextClassifiers)
 class AsyncTextClassifiers(BaseTextClassifiers):
     _model_type = AsyncTextClassifiersModel
