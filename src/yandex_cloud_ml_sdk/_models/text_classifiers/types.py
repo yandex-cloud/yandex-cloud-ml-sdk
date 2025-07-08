8af58564b7aa900472dcbb094b48ba44088af200
Radmila M. <mila.mana@yandex.ru>
2025-07-08 21:30:36 +0300

    Add docstrings for _models/text_classifiers/types.py - v1

--- src/yandex_cloud_ml_sdk/_models/text_classifiers/types.py
+++ src/yandex_cloud_ml_sdk/_models/text_classifiers/types.py
@@ -6,19 +6,32 @@ from typing import Mapping, TypedDict
 
 @dataclass(frozen=True)
 class TextClassificationLabel(Mapping):
+    """This class represents a label for text classification
+    with an associated confidence score.
+    """
+    #: the label for the classification
     label: str
+    #: the confidence score associated with the label
     confidence: float
 
     def __getitem__(self, key):
+        """Retrieve the value associated with the given key.
+        If the key is not found in the label or confidence attributes will raise the error.
+        """
         return asdict(self)[key]
 
     def __iter__(self):
+        """Return an iterator over the items (key-value pairs) of the label."""
         return iter(asdict(self))
 
     def __len__(self):
+        """Return the number of items in the label."""
         return len(asdict(self))
 
 
 class TextClassificationSample(TypedDict):
+    """This class represents a sample of text for classification."""
+    #: the text to be classified
     text: str
+    #: the expected label for the classification
     label: str
