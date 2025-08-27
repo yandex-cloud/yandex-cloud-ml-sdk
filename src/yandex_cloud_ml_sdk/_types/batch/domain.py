614d08fb6e1022a414fb3d1c0f27dc2d856c754c
andy-fisher <andy-fisher@yandex-team.ru>
2025-08-27 11:24:05 +0300

    Revert "Update domain.py"
    This reverts commit 071eb90c1784c2397b45ffcec8ea13b939d8281c.

--- src/yandex_cloud_ml_sdk/_types/batch/domain.py
+++ src/yandex_cloud_ml_sdk/_types/batch/domain.py
@@ -108,4 +108,6 @@ class BatchSubdomain(BaseBatchSubdomain[BatchTaskOperation]):
         )
 
 
+#: Type variable for batch subdomain types, bound to BaseBatchSubdomain.
+#: Used for generic typing of batch subdomain implementations.
 BatchSubdomainTypeT = TypeVar('BatchSubdomainTypeT', bound=BaseBatchSubdomain)
