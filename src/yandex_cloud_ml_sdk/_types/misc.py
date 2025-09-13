e34e91f9cfcd549eda79dd4077408c1af55e252b
andy-fisher <andy-fisher@yandex-team.ru>
2025-09-13 22:29:18 +0300

    revert

--- src/yandex_cloud_ml_sdk/_types/misc.py
+++ src/yandex_cloud_ml_sdk/_types/misc.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import os
 import pathlib
-from typing import Any, TypeVar, Union, cast, overload
+from typing import Any, TypeVar, Union, cast
 
 from typing_extensions import TypeAlias, TypeGuard
 
@@ -26,58 +26,6 @@ UndefinedOr = Union[_T, Undefined]
 
 def is_defined(obj: _T | Undefined) -> TypeGuard[_T]:
     return obj is not UNDEFINED
-
-
-# Overloads for better type inference
-@overload
-def get_defined_value(obj: UndefinedOr[_T], default: None) -> _T | None:
-    ...
-
-
-@overload
-def get_defined_value(obj: UndefinedOr[_T], default: str) -> _T | str:
-    ...
-
-
-@overload
-def get_defined_value(obj: UndefinedOr[_T], default: int) -> _T | int:
-    ...
-
-
-@overload
-def get_defined_value(obj: UndefinedOr[_T], default: float) -> _T | float:
-    ...
-
-
-@overload
-def get_defined_value(obj: UndefinedOr[_T], default: bool) -> _T | bool:
-    ...
-
-
-@overload
-def get_defined_value(obj: UndefinedOr[_T], default: list[Any]) -> _T | list[Any]:
-    ...
-
-
-# Специальный overload для пустого списка
-@overload
-def get_defined_value(obj: UndefinedOr[_T], default: list) -> _T | list:
-    ...
-
-
-@overload
-def get_defined_value(obj: UndefinedOr[_T], default: dict[Any, Any]) -> _T | dict[Any, Any]:
-    ...
-
-
-@overload
-def get_defined_value(obj: UndefinedOr[_T], default: PathLike) -> _T | PathLike:
-    ...
-
-
-@overload
-def get_defined_value(obj: UndefinedOr[_T], default: _D) -> _T | _D:
-    ...
 
 
 def get_defined_value(obj: UndefinedOr[_T], default: _D) -> _T | _D:
