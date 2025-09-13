158985b57e1be4f5b31b4c87939533a31392ce2a
andy-fisher <andy-fisher@yandex-team.ru>
2025-09-13 18:54:17 +0300

    get_defined_value_fix

--- src/yandex_cloud_ml_sdk/_types/tuning/tuning_types.py
+++ src/yandex_cloud_ml_sdk/_types/tuning/tuning_types.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 import abc
 from dataclasses import dataclass
-from typing import ClassVar
+from typing import ClassVar, Type
 
 from yandex.cloud.ai.tuning.v1.tuning_types_pb2 import TuningTypeLora as ProtoTuningTypeLora
 from yandex.cloud.ai.tuning.v1.tuning_types_pb2 import TuningTypePromptTune as ProtoTuningTypePromptTune
@@ -19,14 +19,14 @@ class BaseTuningType(BaseTuningParameter):
 
     @property
     @abc.abstractmethod
-    def proto_type(self) -> ProtoTuningTypeLora | ProtoTuningTypePromptTune:
+    def proto_type(self) -> Type[ProtoTuningTypeLora] | Type[ProtoTuningTypePromptTune]:
         """:meta private:"""
 
 
 @dataclass(frozen=True)
 class TuningTypeLora(BaseTuningType):
     #: :meta private:
-    proto_type: ClassVar = ProtoTuningTypeLora
+    proto_type: ClassVar[Type[ProtoTuningTypeLora]] = ProtoTuningTypeLora
     #: :meta private:
     field_name: ClassVar[str] = 'lora'
 
@@ -39,7 +39,7 @@ class TuningTypeLora(BaseTuningType):
 @dataclass(frozen=True)
 class TuningTypePromptTune(BaseTuningType):
     #: :meta private:
-    proto_type: ClassVar = ProtoTuningTypePromptTune
+    proto_type: ClassVar[Type[ProtoTuningTypePromptTune]] = ProtoTuningTypePromptTune
     #: :meta private:
     field_name: ClassVar[str] = 'prompt_tune'
 
