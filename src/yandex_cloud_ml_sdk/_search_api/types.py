ecf615a77f2a4cb14b17ce48adc1e450a87d0085
Vladimir Lipkin <lipkin@yandex-team.ru>
2025-10-07 19:18:17 +0300

    Add special enum type for wrapping proto enums

---
+++ src/yandex_cloud_ml_sdk/_search_api/types.py
@@ -0,0 +1,18 @@
+# pylint: disable=protected-access,no-name-in-module
+from __future__ import annotations
+
+from yandex.cloud.searchapi.v2.search_query_pb2 import SearchQuery as ProtoSearchQuery
+
+from yandex_cloud_ml_sdk._types.enum import ProtoBasedEnum
+
+
+class SearchType(ProtoBasedEnum):
+    __proto_enum_type__ = ProtoSearchQuery.SearchType
+    __common_prefix__ = 'SEARCH_TYPE_'
+
+    RU = ProtoSearchQuery.SearchType.SEARCH_TYPE_RU
+    TR = ProtoSearchQuery.SearchType.SEARCH_TYPE_TR
+    COM = ProtoSearchQuery.SearchType.SEARCH_TYPE_COM
+    KK = ProtoSearchQuery.SearchType.SEARCH_TYPE_KK
+    BE = ProtoSearchQuery.SearchType.SEARCH_TYPE_BE
+    UZ = ProtoSearchQuery.SearchType.SEARCH_TYPE_UZ
