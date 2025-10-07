a42b26d73cc3753856f68cace149827115e5b62a
Vladimir Lipkin <lipkin@yandex-team.ru>
2025-10-07 19:18:17 +0300

    Add special enum type for wrapping proto enums

---
+++ src/yandex_cloud_ml_sdk/_types/enum.py
@@ -0,0 +1,120 @@
+from __future__ import annotations
+
+from enum import EnumMeta, IntEnum
+from typing import Generic, Protocol, TypeVar, Union
+
+T = TypeVar('T', bound=int)
+ProtoBasedEnumTypeT = TypeVar('ProtoBasedEnumTypeT', bound='ProtoBasedEnum')
+
+
+
+class EnumTypeWrapperProtocol(Protocol[T]):
+    def Name(self, number: T) -> str: ...
+    def Value(self, name: str) -> T: ...
+    def items(self) -> list[tuple[str, T]]: ...
+    def keys(self) -> list[str]: ...
+    def values(self) -> list[T]: ...
+
+
+class UnknownEnumValue(Generic[ProtoBasedEnumTypeT]):
+    def __init__(self, enum_type: type[ProtoBasedEnumTypeT], name: str, value: int):
+        self._enum_type = enum_type
+        self._name = name
+        self._value = value
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def value(self) -> int:
+        return self._value
+
+    @property
+    def enum_type(self) -> type[ProtoBasedEnumTypeT]:
+        return self._enum_type
+
+    def __str__(self) -> str:
+        return f"{self._enum_type.__name__}.Unknown({self._name!r}, {self._value!r})"
+
+    def __repr__(self) -> str:
+        return str(self)
+
+    def __eq__(self, other) -> bool:
+        if isinstance(other, UnknownEnumValue):
+            return (
+                self._enum_type is other._enum_type
+                and self._value == other._value
+            )
+        return False
+
+    def __hash__(self) -> int:
+        return hash((self._enum_type, self._value))
+
+
+class EnumWithUnknownType(EnumMeta):
+    __common_prefix__: str
+    __proto_enum_type__: EnumTypeWrapperProtocol
+
+    def __instancecheck__(cls, inst):
+        if isinstance(inst, UnknownEnumValue):
+            return inst.enum_type is cls
+        return super().__instancecheck__(inst)
+
+
+class ProtoBasedEnum(IntEnum, metaclass=EnumWithUnknownType):
+    __common_prefix__: str
+    __proto_enum_type__: EnumTypeWrapperProtocol
+
+    @classmethod
+    def _coerce(
+        cls: type[ProtoBasedEnumTypeT],
+        value: str | ProtoBasedEnumTypeT | int
+    ) -> EnumWithUnknownAlias[ProtoBasedEnumTypeT]:
+        common_prefix = cls.__common_prefix__
+        proto_enum_type: EnumTypeWrapperProtocol = cls.__proto_enum_type__
+
+        if isinstance(value, cls):
+            return value
+
+        if isinstance(value, int):
+            try:
+                return cls(value)
+            except ValueError:
+                pass
+
+            try:
+                # pylint: disable-next=no-member
+                proto_name = proto_enum_type.Name(value)
+                short_name = proto_name.removeprefix(common_prefix)
+                return cls.Unknown(short_name, value)
+            except ValueError:
+                # pylint: disable-next=raise-missing-from
+                raise ValueError(f'wrong value "{value}" for use as an alias for {cls}')
+
+        if isinstance(value, str):
+            name = value.upper()
+            short_name = name.removeprefix(common_prefix)
+            long_name = name if name.startswith(common_prefix) else common_prefix + name
+
+            if result := getattr(cls, name, None):
+                return result
+            if result := getattr(cls, short_name, None):
+                return result
+
+            try:
+                # pylint: disable-next=no-member
+                proto_value = proto_enum_type.Value(long_name)
+                return cls.Unknown(short_name, proto_value)
+            except ValueError:
+                # pylint: disable-next=raise-missing-from
+                raise ValueError(f'wrong value "{value}" for use as an alias for {cls}')
+
+        raise TypeError(f'wrong type "{type(value)}" for use as an alias for {cls}')
+
+    @classmethod
+    def Unknown(cls, name: str, value: int):
+        return UnknownEnumValue(cls, name, value)
+
+
+EnumWithUnknownAlias = Union[ProtoBasedEnumTypeT, UnknownEnumValue[ProtoBasedEnumTypeT]]
