6f9548f438059aec50e8a5afaff20398187d0fce
Radmila M. <mila.mana@yandex.ru>
2025-10-08 09:27:34 +0300

    Add docstrings for _datasets/task_types.py - v1

--- src/yandex_cloud_ml_sdk/_datasets/task_types.py
+++ src/yandex_cloud_ml_sdk/_datasets/task_types.py
@@ -10,20 +10,32 @@ if TYPE_CHECKING:
 
 
 class KnownTaskType(str, Enum):
+    """Enumeration of known task types."""
+    #: task type for text-to-text generation
     TextToTextGeneration = 'TextToTextGeneration'
+    #: task type for multilabel text classification
     TextClassificationMultilabel = 'TextClassificationMultilabel'
+    #: task type for multiclass text classification
     TextClassificationMulticlass = 'TextClassificationMulticlass'
+    #: task type for speech-to-text generation
     SpeechToTextGeneration = 'SpeechToTextGeneration'
+    #: task type for text-to-speech generation
     TextToSpeechGeneration = 'TextToSpeechGeneration'
+    #: task type for text-to-image generation
     TextToImageGeneration = 'TextToImageGeneration'
+    #: task type for text embeddings in pairs
     TextEmbeddingsPair = 'TextEmbeddingsPair'
+    #: task type for text embeddings in triplets
     TextEmbeddingsTriplet = 'TextEmbeddingsTriplet'
 
+    #: request type for text-to-text generation
     TextToTextGenerationRequest = 'TextToTextGenerationRequest'
+    #: request type for image-to-text generation
     ImageTextToTextGenerationRequest = 'ImageTextToTextGenerationRequest'
 
 
 class TaskTypeProxy:
+    """Proxy class for handling task types."""
     def __init__(self, task_type: KnownTaskType):
         self._task_type = task_type
 
@@ -53,28 +65,34 @@ class TaskTypeProxy:
 
 
 class DatasetsWrapper:
+    """Wrapper class for managing datasets associated with a task type."""
     def __init__(self, task_type: str, domain: Datasets | AsyncDatasets):
         self._task_type = task_type
         self._domain = domain
 
     @property
     def task_type(self) -> str:
+        """A function for the task type managed by the wrapper."""
         return self._task_type
 
     @property
     def draft_from_path(self):
+        """A function for drafting from a path based on the task type."""
         return partial(self._domain.draft_from_path, task_type=self._task_type)
 
     @property
     def list_upload_formats(self):
+        """A function for listing upload formats based on the task type."""
         return partial(self._domain.list_upload_formats, task_type=self._task_type)
 
     @property
     def list_upload_schemas(self):
+        """A function for listing upload schemas based on the task type."""
         return partial(self._domain.list_upload_schemas, task_type=self._task_type)
 
     @property
     def list(self):
+        """A function for listing datasets based on the task type."""
         return partial(self._domain.list, task_type=self._task_type)
 
     def __repr__(self) -> str:
