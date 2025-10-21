85d652f229f801872b16c6c9eeeac14255ac4934
Vladimir Lipkin <lipkin@yandex-team.ru>
2025-10-21 17:48:21 +0200

    Add web search function (#168)
    * Add special enum type for wrapping proto enums
    
    * Add web search enums
    
    * Add image search enums
    
    * Add web search

---
+++ examples/sync/search_api/web/run_deferred.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+from __future__ import annotations
+
+from yandex_cloud_ml_sdk import YCloudML
+
+
+def main() -> None:
+    sdk = YCloudML(folder_id='b1ghsjum2v37c2un8h64')
+    sdk.setup_default_logging()
+
+    search = sdk.search_api.web('RU')
+
+    search_query = input('Enter the search query: ')
+    if not search_query.strip():
+        search_query = 'Yandex Cloud'
+
+    operation = search.run_deferred(search_query)
+    # More on how to work with "search_result" objects at
+    # run_simple.py
+    search_result = operation.wait(poll_interval=1)
+    print(f'{len(search_result.docs)} documents found on page #{0}')
+
+    # Search result have a helper to request a next page:
+    operation = search_result.next_page_deferred()
+    search_result = operation.wait(poll_interval=1)
+    print(f'{len(search_result.docs)} documents found on page #{1}')
+
+    # More on how to work with "raw" formats at run_raw.py example;
+    # here is only about run_deferred
+    operation_xml = search.run_deferred(search_query, format='xml')
+    # You could poll operation more often:
+    bytes_search_result = operation_xml.wait(poll_interval=1)
+    print(f'{len(bytes_search_result)} bytes in resulting xml for page #{0}')
+
+    # there is no helpers to request next page for raw format:
+    operation_xml = search.run_deferred(search_query, format='xml', page=1)
+    bytes_search_result = operation_xml.wait(poll_interval=1)
+    print(f'{len(bytes_search_result)} bytes in resulting xml for page #{1}')
+
+
+if __name__ == '__main__':
+    main()
