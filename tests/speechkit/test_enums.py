3cad351c858efe4c8b99cafb223422f9abb6eee7
Vladimir Lipkin <lipkin@yandex-team.ru>
2025-12-05 18:01:23 +0100

    Add the rest of speechkit enums (#182)

--- tests/speechkit/test_enums.py
+++ tests/speechkit/test_enums.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import pytest
 
-from yandex_cloud_ml_sdk._speechkit.enums import PCM16, AudioFormat
+from yandex_cloud_ml_sdk._speechkit.enums import PCM16, AudioFormat, LanguageCode
 
 
 @pytest.mark.parametrize(
@@ -18,7 +18,8 @@ from yandex_cloud_ml_sdk._speechkit.enums import PCM16, AudioFormat
         ('LINEAR16_PCM(400,4)', PCM16(400, 4)),
         (AudioFormat.MP3, AudioFormat.MP3),
         (AudioFormat.PCM16(400), PCM16(400, 1)),
-])
+    ]
+)
 def test_audio_format_coerce(input_, etalon):
     assert AudioFormat._coerce(input_) == etalon
 
@@ -31,7 +32,33 @@ def test_audio_format_coerce(input_, etalon):
         ('PCM16(1,  2)', ValueError),
         ('PCM16(100, 200, 300)', ValueError),
         ({}, TypeError),
-])
+    ]
+)
 def test_audio_format_exception(input_, exc_type):
+    with pytest.raises(exc_type):
+        AudioFormat._coerce(input_)
+
+
+@pytest.mark.parametrize(
+    'input_,etalon', [
+        (LanguageCode.ru_RU, 'ru-RU'),
+        (LanguageCode.auto , 'auto'),
+        ('RU_ru', 'ru-RU'),
+        ('FOO_bAr', 'foo-BAR'),
+    ]
+)
+def test_language_code_coerce(input_, etalon):
+    assert LanguageCode._coerce_to_str(input_) == etalon
+
+
+@pytest.mark.parametrize(
+    'input_,exc_type', [
+        ({}, TypeError),
+        ('foo-bar-baz', ValueError),
+        ('-foo-bar', ValueError),
+        ('foo', ValueError)
+    ]
+)
+def test_language_code_exception(input_, exc_type):
     with pytest.raises(exc_type):
         AudioFormat._coerce(input_)
