fadf93924b8ab31482d980ea3303565a6eb3cb37
GBocharov <bocharovgleb@yandex-team.ru>
2025-12-23 17:37:36 +0300

    fix-examples

--- examples/sync/search_api/generative/integrations.py
+++ examples/sync/search_api/generative/integrations.py
@@ -28,7 +28,7 @@ def main() -> None:
 
     # 3) We are passing search result to a yandex-art with an extra instruction
     image_model = sdk.models.image_generation('yandex-art')
-    operation = image_model.run_deferred(["Draw a character by next instruction:", search_result.text])
+    operation = image_model.run_deferred(["Draw a character by next instruction:", search_result])
     image_result = operation.wait()
     try:
         pathlib.Path('image.jpeg').write_bytes(image_result.image_bytes)
